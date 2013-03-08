9e011cf1902615e47375be6ef06f587164796b48
Fabian Nicollier <fabian.nicollier@ed47.com>
2013-03-08 17:02:58 +0100

    Updated sample to include domains, entity and more.

---
+++ ED47.Stack.Sample/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Optimization;
+using ED47.Stack.Sample.App_Start;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(BundleConfig), "Start")]
+namespace ED47.Stack.Sample.App_Start
+{
+    public class BundleConfig
+    {
+        public static void Start()
+        {
+            BundleTable.Bundles.Add(new ScriptBundle("~/jsBundle")
+                .Include(
+                    "~/Scripts/jquery-*",
+                    "~/Scripts/jquery.*"
+                    //TODO: Add your general scripts here
+                )
+            );
+
+            BundleTable.Bundles.Add(new StyleBundle("~/cssBundle")
+                .Include(
+                    "~/Content/Styles/html5reset*"
+                    //TODO: Add your styles here
+               )
+            );
+            
+            /*
+             * Add here your ExtJS-based per page JS bundles
+             * 
+            BundleTable.Bundles.Add(new ScriptBundle("~/jsXXXX")
+                .IncludeDirectory("~/js/YYYY/views/XXXX", "*.js"));*/
+            
+#if !DEBUG
+            //Forces optimization for testing purposes
+            BundleTable.EnableOptimizations = true;
+#endif
+        }
+    }
+}
