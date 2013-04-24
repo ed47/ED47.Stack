bf4cdbafd5fc45fa1540b37950f9bd5ab911e78f
Fabian Nicollier <fabian.nicollier@ed47.com>
2013-04-24 13:55:44 +0200

    Add FluentValidation Nuget package.
    Refactored diff helper method to use a strongly typed object.

--- ED47.BusinessAccessLayer/BusinessEntityJson.cs
+++ ED47.BusinessAccessLayer/BusinessEntityJson.cs
@@ -3,46 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using Newtonsoft.Json.Linq;
+using FluentValidation.Results;
 
 namespace ED47.BusinessAccessLayer
 {
     public static class BusinessEntityJson
     {
-        public static JObject JsonDifferences(this BusinessEntity original, BusinessEntity updatedObject)
+        public static UpdateResult Differences(this BusinessEntity original, BusinessEntity updatedObject)
         {
-            var json = new JObject();
+            var results = new UpdateResult();
             var type = original.GetType();
             var properties = type
                 .GetProperties(BindingFlags.Instance | BindingFlags.Public)
                 .Where(el => el.Name != "ClientData" && !el.GetCustomAttributes(typeof(IgnoreDataMemberAttribute), true).Any());
-
+            
             foreach (var propertyInfo in properties)
             {
                 var originalValue = propertyInfo.GetValue(original, null);
                 var updatedValue = propertyInfo.GetValue(updatedObject, null);
-                JToken JValue;
 
                 if (Object.Equals(originalValue, updatedValue))
                     continue;
 
-                if (propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.IsInterface &&
-                    propertyInfo.PropertyType.GetGenericTypeDefinition() == typeof(ICollection<>))
-                {
-                    JValue = new JArray(updatedValue);
-                }
-                else
-                {
-                    if (Object.Equals(originalValue, updatedValue))
-                        continue;
-
-                    JValue = new JValue(updatedValue);
-                }
-
-                json.Add(propertyInfo.Name, JValue);
+                results.Values.Add(propertyInfo.Name, updatedValue);
             }
 
-            return json;
+            return results;
+        }
+    }
+    
+    public class UpdateResult
+    {
+        public Dictionary<string, object> Values { get; set; }
+        public IEnumerable<FluentValidation.Results.ValidationFailure> Validations { get; set; }
+
+        public UpdateResult()
+        {
+            Values = new Dictionary<string, object>();
+            Validations = new List<ValidationFailure>();
         }
     }
 }
