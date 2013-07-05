821467ac308b8ec971af95b460e248976cf40425
Fabian Nicollier <fabian.nicollier@ed47.com>
2013-07-05 09:52:16 +0200

    Excel export: minor optimization.

--- ED47.BusinessAccessLayer/Excel/ExcelColumn.cs
+++ ED47.BusinessAccessLayer/Excel/ExcelColumn.cs
@@ -3,22 +3,18 @@ using OfficeOpenXml;
 
 namespace ED47.BusinessAccessLayer.Excel
 {
-
-    
-    
     public class ExcelColumn 
     {
-        public ExcelColumn()
-        {
-            Renderer = (el,range) => range.Value = el;
-        }
-        
         public void Render(object value, ExcelRange range, Action<object, ExcelRange> renderer = null)
         {
-            var r = renderer ?? Renderer;
-            r(value, range);
-         
+            renderer = renderer ?? Renderer;
+
+            if (renderer == null)
+                range.Value = value;
+            else
+                renderer(value, range);
         }
+
         public string PropertyName { get; set; }
         public string DisplayName { get; set; }
         public Action<object, ExcelRange> Renderer { get; set; }
