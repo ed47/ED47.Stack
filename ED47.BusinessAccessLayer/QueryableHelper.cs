b11280b2c8c4efa1f4e95d73e55b1c670133d10e
Zwahlen Alexandre <alexandre.zwahlen@ed47.com>
2013-10-22 13:39:48 +0200

    IQueryable extension for string order by

---
+++ ED47.BusinessAccessLayer/QueryableHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace ED47.BusinessAccessLayer
+{
+    public static class QueryableHelper
+    {
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering, string direction) 
+        {
+            var type = typeof(T);
+            var property = type.GetProperty(ordering);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExp = Expression.Lambda(propertyAccess, parameter);
+
+            MethodCallExpression resultExp = Expression.Call(typeof(Queryable), direction == "ASC" ? "OrderBy" : "OrderByDescending", new Type[] { type, property.PropertyType }, source.Expression, Expression.Quote(orderByExp));
+            return source.Provider.CreateQuery<T>(resultExp);
+        }
+    }
+}
