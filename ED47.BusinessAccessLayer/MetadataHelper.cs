0efe3de573418648c52c250aa1b22df4de851ecf
Fabian Nicollier <fabian.nicollier@ed47.com>
2013-12-19 16:28:14 +0100

    EF6

--- ED47.BusinessAccessLayer/MetadataHelper.cs
+++ ED47.BusinessAccessLayer/MetadataHelper.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
-using System.Data.Objects;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
