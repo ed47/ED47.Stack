8b079fd9ab5df4845ae1e1a729768170ef03c47b
Fabian Nicollier <fabian.nicollier@ed47.com>
2014-01-17 11:55:39 +0100

    Created SQL repository interface.

--- ED47.BusinessAccessLayer.EF/Repository.cs
+++ ED47.BusinessAccessLayer.EF/Repository.cs
@@ -12,35 +12,7 @@ using Omu.ValueInjecter;
 
 namespace ED47.BusinessAccessLayer.EF
 {
-    public interface ISqlRepository : IRepository
-    {
-        /// <summary>
-        /// The current Entity Framework DbContext.
-        /// </summary>
-        DbContext DbContext { get; set; }
-
-        IEnumerable<TBusinessEntity> ExecuteTableFunction<TBusinessEntity>(string tableFunction, params object[] parameters) where TBusinessEntity : class;
-        IEnumerable<TBusinessEntity> ExecuteStoredProcedure<TBusinessEntity>(string storedProcedure, params object[] parameters) where TBusinessEntity : class;
-        IEnumerable<TBusinessEntity> ExecuteStoredProcedure<TBusinessEntity>(string storedProcedure, params SqlParameter[] parameters) where TBusinessEntity : class;
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "SQL query is parametrized")]
-        int ExecuteNonQuery(string storedProcedure, params SqlParameter[] parameters);
-
-        /// <summary>
-        /// Execute a stored procedure and apply the result on the collection
-        /// </summary>
-        /// <typeparam name="TEntity">The type of the entity.</typeparam>
-        /// <param name="storedProcedure">The stored procedure.</param>
-        /// <param name="idParameter">The id parameter.</param>
-        /// <param name="entities">The entities.</param>
-        /// <param name="keySelector">The key selector.</param>
-        /// <param name="entitySelector">The entity selector.</param>
-        /// <param name="dataSelector">The data selector.</param>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "SQL query is parametrized.")]
-        void ApplyStoredProcedure<TEntity>(string storedProcedure, string idParameter, IEnumerable<TEntity> entities, Func<TEntity, IntKey> keySelector = null, Func<TEntity, object> entitySelector = null, Func<SqlDataReader, object> dataSelector = null, IEnumerable<SqlParameter> parameters = null);
-    }
-
-    public class Repository : IRepository, ISqlRepository
+    public class Repository : ISqlRepository
     {
         /// <summary>
         /// The current Entity Framework DbContext.
