bb88b1d2bb71f38584a419852eaf9cf27531a93c
Fabian Nicollier <fabian.nicollier@ed47.com>
2014-02-17 10:19:40 +0100

    Updated packages

--- ED47.BusinessAccessLayer.EF/MultilingualRepository.cs
+++ ED47.BusinessAccessLayer.EF/MultilingualRepository.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
-using System.Data.Objects;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
--- ED47.Stack.Reflector/Properties/Settings.Designer.cs
+++ ED47.Stack.Reflector/Properties/Settings.Designer.cs
@@ -9,11 +9,12 @@
 //------------------------------------------------------------------------------
 
 namespace ED47.Stack.Reflector.Properties {
-    
-    
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
