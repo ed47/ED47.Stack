d634a2383bbc5844aa087211fa95390bf1aaab56
Fabian Nicollier <fabian.nicollier@ed47.com>
2014-02-27 10:18:19 +0100

    Added UnitOfWork to encapsulate using multiple repositories with the same DbContext.

---
+++ ED47.BusinessAccessLayer/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ED47.BusinessAccessLayer
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        DbContext DbContext { get; }
+        DbContext ImmediateDbContext { get; }
+
+        void Commit();
+    }
+}
