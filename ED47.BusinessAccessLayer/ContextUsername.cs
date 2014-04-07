26c783f6cde89e23614cfffcfda01da3d2e6dfb6
Fabian Nicollier <fabian.nicollier@ed47.com>
2014-04-07 16:47:44 +0200

    ContextUsername: handle when there is a HttpContext with no user (call outside of an user call)

--- ED47.BusinessAccessLayer/ContextUsername.cs
+++ ED47.BusinessAccessLayer/ContextUsername.cs
@@ -6,7 +6,7 @@ namespace ED47.BusinessAccessLayer
     {
         public static string Get()
         {
-            if (HttpContext.Current == null || !HttpContext.Current.User.Identity.IsAuthenticated)
+            if (HttpContext.Current == null || HttpContext.Current.User == null || !HttpContext.Current.User.Identity.IsAuthenticated)
                 return "anonymous";
 
             return HttpContext.Current.User.Identity.Name;
