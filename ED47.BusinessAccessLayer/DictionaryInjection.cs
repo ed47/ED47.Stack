d85f0f70cdd913e6caad02fdf18fedaf3d33f5bc
Fabian Nicollier <fabian.nicollier@ed47.com>
2014-04-25 13:18:28 +0200

    DictionaryInjection: handle enumerations. Also refactored because of gigantic method.

--- ED47.BusinessAccessLayer/DictionaryInjection.cs
+++ ED47.BusinessAccessLayer/DictionaryInjection.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Data;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using Newtonsoft.Json.Linq;
 using Omu.ValueInjecter;
 
@@ -12,7 +11,7 @@ namespace ED47.BusinessAccessLayer
     /// <summary>
     /// Injection from a dictionnary to a known type.
     /// </summary>
-    public class DictionaryInjection : KnownSourceValueInjection<ICollection<KeyValuePair<string,object>>>
+    public class DictionaryInjection : KnownSourceValueInjection<ICollection<KeyValuePair<string, object>>>
     {
         protected override void Inject(ICollection<KeyValuePair<string, object>> source, object target)
         {
@@ -24,68 +23,93 @@ namespace ED47.BusinessAccessLayer
 
                 if (value as JObject != null) continue;
 
-                if(value != null && !targetProp.PropertyType.IsNullable() && targetProp.PropertyType !=value.GetType() && (targetProp.PropertyType.IsPrimitive || targetProp.PropertyType == typeof(decimal)))
+                if (value != null && !targetProp.PropertyType.IsNullable() && targetProp.PropertyType != value.GetType() && (targetProp.PropertyType.IsPrimitive || targetProp.PropertyType == typeof(decimal)))
                 {
                     value = Convert.ChangeType(value, targetProp.PropertyType);
                 }
 
-                if (targetProp.PropertyType.IsNullable() && (value != null && Nullable.GetUnderlyingType(targetProp.PropertyType) != value.GetType()))
-                {
-                    if (value.GetType().Name == "String" && String.IsNullOrWhiteSpace(value.ToString()))
-                        value = null;
-                    else
-                        value = Convert.ChangeType(value, Nullable.GetUnderlyingType(targetProp.PropertyType));
-                }
+                value = ConvertNullable(targetProp, value);
 
                 if (value == String.Empty && targetProp.PropertyType != typeof(String))
                     value = null;
 
-                if (value is JArray)
-                {
-                    var jArray = (JArray)value;
-                    
-                    if (jArray.Any())
-                    {
-                        if (jArray.First().Type == JTokenType.Integer)
-                            value = jArray.Select(el => (int)el).ToArray();
+                value = ReadJArray(value);
 
-                        if (jArray.First().Type == JTokenType.String)
-                            value = jArray.Select(el => (string)el).ToArray();
-                    }
-                }
-
-                if (targetProp.PropertyType.IsGenericType && targetProp.PropertyType.IsInterface && targetProp.PropertyType.GetGenericTypeDefinition() == typeof(ICollection<>))
-                {
-                    if (targetProp.GetValue(target) == null)
-                    {
-                        var collection = typeof (Collection<>);
-                        var typeArgs = targetProp.PropertyType.GetGenericArguments();
-                        targetProp.SetValue(target, Activator.CreateInstance(collection.MakeGenericType(typeArgs)));
-                    }
-
-                    if (value == null)
-                        continue;
-
-                    var values = ((string) value).Split(',');
-                    var collectionType = targetProp.PropertyType.GetGenericArguments().First();
-
-                    foreach (var collectionValue in values)
-                    {
-                        object convertedValue = collectionValue;
-
-                        if (collectionType != typeof (string))
-                        {
-                            convertedValue = Convert.ChangeType(convertedValue, collectionType);
-                        }
-
-                        targetProp.PropertyType.GetMethod("Add").Invoke(targetProp.GetValue(target), new object[] { convertedValue });   
-                    }
-                    
-                    continue;
-                }
-
-                targetProp.SetValue(target, value);
+                SetValue(target, targetProp, value);
             }
+        }
+
+        private static void SetValue(object target, PropertyDescriptor targetProp, object value)
+        {
+            if (targetProp.PropertyType.IsGenericType && targetProp.PropertyType.IsInterface &&
+                targetProp.PropertyType.GetGenericTypeDefinition() == typeof (ICollection<>))
+            {
+                if (targetProp.GetValue(target) == null)
+                {
+                    var collection = typeof (Collection<>);
+                    var typeArgs = targetProp.PropertyType.GetGenericArguments();
+                    targetProp.SetValue(target, Activator.CreateInstance(collection.MakeGenericType(typeArgs)));
+                }
+
+                if (value == null)
+                    return;
+
+                var values = ((string) value).Split(',');
+                var collectionType = targetProp.PropertyType.GetGenericArguments().First();
+
+                foreach (var collectionValue in values)
+                {
+                    object convertedValue = collectionValue;
+
+                    if (collectionType != typeof (string))
+                        convertedValue = Convert.ChangeType(convertedValue, collectionType);
+
+                    targetProp.PropertyType.GetMethod("Add").Invoke(targetProp.GetValue(target), new object[] {convertedValue});
+                }
+
+                return;
+            }
+
+            targetProp.SetValue(target, value);
+        }
+
+        private static object ReadJArray(object value)
+        {
+            if (value is JArray)
+            {
+                var jArray = (JArray) value;
+
+                if (jArray.Any())
+                {
+                    if (jArray.First().Type == JTokenType.Integer)
+                        value = jArray.Select(el => (int) el).ToArray();
+
+                    if (jArray.First().Type == JTokenType.String)
+                        value = jArray.Select(el => (string) el).ToArray();
+                }
+            }
+            return value;
+        }
+
+        private static object ConvertNullable(PropertyDescriptor targetProp, object value)
+        {
+            if (targetProp.PropertyType.IsNullable() &&
+                (value != null && Nullable.GetUnderlyingType(targetProp.PropertyType) != value.GetType()))
+            {
+                if (value.GetType().Name == "String" && String.IsNullOrWhiteSpace(value.ToString()))
+                    value = null;
+                else
+                {
+                    if (targetProp.PropertyType.BaseType == typeof (Enum))
+                        value = Enum.Parse(targetProp.PropertyType, (string) value);
+                    else if (targetProp.PropertyType.IsGenericType &&
+                             targetProp.PropertyType.GetGenericArguments().First().BaseType == typeof (Enum))
+                        value = Enum.Parse(targetProp.PropertyType.GetGenericArguments().First(), (string) value);
+                    else
+                        value = Convert.ChangeType(value, Nullable.GetUnderlyingType(targetProp.PropertyType));
+                }
+            }
+            return value;
         }
     }
 
