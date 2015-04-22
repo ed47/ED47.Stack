08fe387af72521e345585755ee26b05b19d3fae5
asfez <arie.sfez@essence.ch>
2015-04-22 15:34:42 +0200

    Add guid in IFileBox

--- ED47.BusinessAccessLayer/BusinessEntities/IFileBox.cs
+++ ED47.BusinessAccessLayer/BusinessEntities/IFileBox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Web;
@@ -13,7 +14,7 @@ namespace ED47.BusinessAccessLayer.BusinessEntities
 
         [MaxLength(500)]
         string Path { get; set; }
-
+        Guid Guid { get; set; }
         IEnumerable<FileBoxItem> GetFiles();
         FileBoxItem AddFile(HttpPostedFileBase file, string businessKey, int? groupdId = null, string comment = null, string langId = null, bool requireLogin = true, dynamic metadata = null);
         FileBoxItem AddFile(IFile file, string comment = null);
