9025a8474f2178af3200abde1cb6722733894742
asfez <arie.sfez@essence.ch>
2015-11-09 16:51:50 +0100

    Autolink exclude

--- ED47.Stack.Reflector/Scripts/autolink.js
+++ ED47.Stack.Reflector/Scripts/autolink.js
@@ -1,51 +1,50 @@
-﻿;(function ( $, window, document, undefined ) {
+﻿; (function ($, window, document, undefined) {
 
     var pluginName = "autolink",
         defaults = {
-         
+
         };
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
-         this.options = $.extend( {}, defaults, options );
+        this.options = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
     }
 
     Plugin.prototype = {
+        init: function () {
 
-        init: function() {
-             
             var view = $(this.element);
             var self = this;
-            view.on("click",function(e) {
+            view.on("click", function (e) {
+                
+                if ($(e.target).hasClass("autolink-exclude"))
+                    return;
+
                 e.preventDefault();
+                
                 if (view.data("target") != null) {
                     window.open(view.data("href"), view.data("target"));
                 } else {
                     window.location = view.data("href");
                 }
             });
-            
-         
         }
-        
-      
-        
     };
 
-    $.fn[pluginName] = function ( options ) {
+    $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
             }
         });
     };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
 
-$(function() {
-    
+$(function () {
+
     $(".autolink").livequery(function () {
         $(this).autolink();
         $(this).css("cursor", "pointer");
